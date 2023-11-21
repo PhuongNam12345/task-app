8c0d911550830c88823830618c179cbaebf1d9ac
nam dang <namdang020201@gmail.com>
2023-11-21 21:07:20 +0700

    update feat task

--- model/user_model.go
+++ model/user_model.go
@@ -7,17 +7,6 @@ type User struct {
 	Phone    string `json:"phone"  `
 	Address  string `json:"address" binding:"required"`
 	Role     string `json:"role"  binding:"required"`
-<<<<<<< HEAD
-}
-type Task struct {
-	TaskID   string `json:"taskid" binding:"required"`
-	Taskname string `json:"taskname"   binding:"required"`
-	Starting string `json:"starting" binding:"required" `
-	Deadline string `json:"deadline"  `
-	Catelogy string `json:"catelogy" binding:"required"`
-	UserID   string `json:"userid"  binding:"required"`
-=======
->>>>>>> 31be63cbb1e09fe917034d1d666f5c6462168e61
 }
 type Task struct {
 	TaskID   string `json:"taskid" binding:"required"`
